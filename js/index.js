5d3abc6dfd5d83e7ce99191c13c04766d3555daa
birugh <notblue.https@gmail.com>
2025-11-09 10:08:36 +0700

    add: clickable hamburger

--- js/index.js
+++ js/index.js
@@ -7,4 +7,11 @@ document.addEventListener('DOMContentLoaded', () => {
             item.classList.add('selected');
         });
     });
-})
+
+    const hamburger = document.getElementById('hamburger-nav');
+    const menu = document.getElementById('menu-nav');
+    
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    })
+})  
